5ba527d0a27d8a09c2e6de22ad0594da49008c2f
johnferneymontoyaramirez <jfmontoya@ins.com.co>
2023-11-14 20:13:12 -0500

    Initial commit

---
+++ src/main/java/john/ejercicio8_2/Ejercicio8_2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package john.ejercicio8_2;
+
+/**
+ *
+ * @author johnferneymontoyaramirez
+ */
+public class Ejercicio8_2 {
+
+    public static void main(String[] args) {
+        VentanaPrincipal form = new VentanaPrincipal();
+	form.setVisible(true);
+    }
+}
